5a9bb9eb7118362571bab4a6523790b7de137ee1
Aliyu Kamilu <aliyukamilu002@gmail.com>
2022-01-21 03:55:22 +0100

    initial commit

---
+++ client/src/components/Sliders.jsx
@@ -0,0 +1,59 @@
+import React, { useEffect, useState, useRef } from 'react';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+import Slider from "react-slick";
+
+const sliderContents = [
+
+  {
+    title: "Slide #1",
+    content: "solana.svg"
+  },
+  {
+    title: "Slide #2",
+    content: "serum.svg"
+  },
+  {
+    title: "Slide #4",
+    content: "coinmarket.svg"
+  },
+  {
+    title: "Slide #5",
+    content: "raydium.svg"
+  },
+  {
+    title: "Slide #6",
+    content: "dexlab.svg"
+  }
+
+]
+
+function Sliders() {
+
+  var settings = {
+    dots: false,
+    arrows:  false,
+    infinite: true,
+    autoplay: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1
+  };
+
+  return (
+    <div className="bg-whito py-12 flex justify-center">
+      <div className="w-8/12">
+        <Slider {...settings}>
+          {sliderContents.map((sliderContent, index) => (
+            <div key={index}>
+              <img src={require(`../images/${sliderContent.content}`)} alt="" className="w-24 h-20" />
+            </div>
+          ))}
+        </Slider>
+      </div>
+    </div>
+  )
+}
+
+export default Sliders;
---
+++ client/src/components/index.js
@@ -0,0 +1,3 @@
+export { default as Navbar } from "./Navbar";
+export { default as Welcome } from "./Welcome";
+export { default as Sliders } from "./Sliders";
