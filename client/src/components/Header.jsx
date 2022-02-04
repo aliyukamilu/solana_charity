2a65e9cd59b4866dea3f87f5a05faf876a0e8435
Aliyu Kamilu <aliyukamilu002@gmail.com>
2022-02-04 07:08:26 +0100

    dashboard, send and swap form

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import Button from './Button'
+import { shortenAdd } from '../utils/shortenAdd';
+
+import { ReactComponent as SolanaLogo } from '../images/solanaWhite.svg';
+
+
+const Header = () => {
+  const [walletConnected, setwalletConnected] = React.useState(false);
+  let address = '0x52EdB5dFCE4640644AD366b660ee9b78872b16c3'
+  return (
+    <div className="gradient-bg-welcome flex w-full px-20 h-20 shadow-xl fixed z-10">
+      <div className="flex flex-row justify-between w-full items-center">
+        <div className="md:flex-[0.5] justify-center items-center">
+          <img src={require('../images/logo.png')} className="w-32 cursor-pointer" alt="" />
+        </div>
+        <div className="flex flex-1 justify-end items-center">
+          <div className="mr-3">
+            <SolanaLogo style={{ width: 100 }} />
+          </div>
+          {walletConnected ?
+            <>
+              <div className="flex flex-row items-center">
+                <Button title={'0 SOL'} customClass="white-glassmorphism rounded-lg py-2 px-3 mr-3" />
+                <Button title={shortenAdd(address)} customClass="bg-primary rounded-lg py-2 px-3" />
+              </div>
+
+            </> :
+            <>
+              <Button title="Connect Wallet" customClass="bg-primary" />
+            </>}
+
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Header;
--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,18 +1,30 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import '../pages/Contract.css'
+
+import { MdOutlineDashboard } from 'react-icons/md'
+import { RiCoinsLine } from 'react-icons/ri'
+import { IoSendOutline } from 'react-icons/io5'
+import { FaParachuteBox } from 'react-icons/fa'
+
+const icons = [
+    <MdOutlineDashboard fontSize={24} className="navIcon" />,
+    <RiCoinsLine fontSize={24} className="navIcon" />,
+    <IoSendOutline fontSize={24} className="navIcon" />,
+    <FaParachuteBox fontSize={24} className="navIcon" />
+]
 
 const Sidebar = () => {
     return (
         <div className="sidebarContainer px-5">
             <div className="navItemContainer">
-                {['Dashboard', 'Swap', 'Send', 'Airdrops'].map((navItem) => (
-                    <Link to="/contract" className="navItem flex items-center mb-8">
+                {['Dashboard', 'Swap', 'Send', 'Airdrops'].map((navItem, index) => (
+                    <NavLink key={index} to={`/${navItem.toLowerCase()}`} className={(navData) => navData.isActive ? "navItem flex items-center mb-4 ACTIVE" : "navItem flex items-center mb-4"}>
                         <div className="iconCont mr-5 rounded-full p-2">
-                            <img src={require('../images/airdrop.png')} className="navIcon w-8" alt="" />
+                            {icons[index]}
                         </div>
                         <p className="navItemTitle">{navItem}</p>
-                    </Link>
+                    </NavLink>
                 ))}
 
             </div>
